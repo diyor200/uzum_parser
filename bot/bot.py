34c4c73abd19eb870ecc575d7d4c2dcad3b5532b
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-06-10 17:13:46 +0500

    final result for now

--- bot/bot.py
+++ bot/bot.py
@@ -2,11 +2,14 @@ import asyncio
 import logging
 import sys
 from os import getenv
+import requests
 
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message
 
 # Bot token can be obtained via https://t.me/BotFather
@@ -30,20 +33,44 @@ async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
 
-@dp.message()
-async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
+class UrlState(StatesGroup):
+    URL = State()
 
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+@dp.message(Command("parse"))
+async def parser_handler(message: Message, state: FSMContext) -> None:
+    await message.answer(
+        text="product url ini yuboring:\n\nMisol:https://uzum.uz/uz/product/new-balance-erkaklar-krossovkalari-1552187"
+    )
+    await state.set_state(UrlState.URL)
 
+
+@dp.message(UrlState.URL)
+async def parser_handler(message: Message, state: FSMContext) -> None:
+    url = message.text
+
+    await message.answer("so'rov yuborilmoqda iltimos kutib turing...")
+    resp = requests.post(url="http://scraper:8000/parse_product", json={"url": url})
+    data = resp.json()
+
+    product = data.get("data", {})
+    text = (
+        f"<b>{product.get('title')}</b>\n"
+        f"⭐️:️ Reyting: {product.get('rating')}\n"
+        f"💸: Narx: {product.get('price')} so'm {product.get('discount')}\n"
+        f"🏬: Sotuuvchi: {product['seller']['title']} (:star:️ {product['seller']['rating']})\n"
+        f"📦: Qolgan mahsulotlar: {product.get('available_amount')}\n"
+        f"🛍: Sotuvlar soni (hafta): {product.get('sold_count')}\n\n"
+        "<b>📏: O'lchamlar va mavjud soni:</b>\n"
+    )
+    for item in product.get("products", []):
+        text += f"• {item['size']} — {item['available_count']} dona\n"
+    images = product.get("images", [])
+    if images:
+        text += "\n<b>🖼: Rasm URL'lari:</b>\n"
+        text += "\n".join(images)
+
+    await message.answer(text=text)
+    await state.clear()
 
 async def main() -> None:
     # Initialize Bot instance with default bot properties which will be passed to all API calls
