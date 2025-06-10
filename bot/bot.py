e8bf3ca9405f276c92ef6f7ba30a6279bc85977c
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-06-10 19:28:06 +0500

    added bot token

--- bot/bot.py
+++ bot/bot.py
@@ -13,7 +13,7 @@ from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message
 
 # Bot token can be obtained via https://t.me/BotFather
-TOKEN = getenv("BOT_TOKEN")
+TOKEN = "7274490167:AAFlQOx1U4BNcwe-3uJ8D6H7ZYwpkTrwI8c"
 
 # All handlers should be attached to the Router (or Dispatcher)
 
